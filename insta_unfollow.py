87df3acc3fcf5f7fba52f9779386dd7a7dc67fa6
Chief Keef <56800387+batiscuff@users.noreply.github.com>
2020-09-13 22:53:49 +0300

    Update insta_unfollow.py

--- insta_unfollow.py
+++ insta_unfollow.py
@@ -25,10 +25,11 @@ class InstagramUnfollowers:
                 if i >= self.total_non_followed:
                     break
                 # Удаляет последнего юзера по никнейму
-                user = list(self.non_follow.keys())[len(self.non_follow) - 1]
+                user = list(non_follow.keys())[len(non_follow) - 1]
                 # Отписывается и удаляет юзера pop() методом
                 self.api.unfollow(non_follow[user])
                 non_follow.pop(user)
+            print("Вы отписались от всех")
 
     def results(self):
         results = [
@@ -98,4 +99,4 @@ if __name__ == "__main__":
     print("------- Введите ваши данные --------")
     USERNAME = input("Введите ваш логин: ")
     PASSWORD = input("Введите ваш пароль: ")
-    print(InstagramUnfollowers(USERNAME, PASSWORD))
+    InstagramUnfollowers(USERNAME, PASSWORD)
